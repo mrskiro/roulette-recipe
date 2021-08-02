48d418353de6bf94a764f7416174296db0ccc5b9
purpleeeee <h.purpleeeee@gmail.com>
2021-08-02 22:28:42 +0900

    chore: move src/

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styled from "styled-components"
+import styles from "../styles/Home.module.css"
+
+const Hoge = styled.h1``
 
 export default function Home() {
   return (
@@ -12,12 +15,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Hoge className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Hoge>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,7 +61,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
